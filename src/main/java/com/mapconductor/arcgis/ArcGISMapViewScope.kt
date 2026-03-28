0f7b61e5212bf05c3e2d29e169172029e544f989
Masashi Katsumata <wf9a5m75@gmail.com>
2026-03-29 04:15:58 +0900

    Port from the android-sdk

---
+++ src/main/java/com/mapconductor/arcgis/ArcGISMapViewScope.kt
@@ -0,0 +1,7 @@
+package com.mapconductor.arcgis.map
+
+import com.mapconductor.core.MapViewScope
+
+class ArcGISMapViewScope : MapViewScope() {
+    // 他の地図SDKにはない機能は、ここで定義する
+}
---
+++ src/main/java/com/mapconductor/arcgis/ArcGISViewControllerStore.kt
@@ -0,0 +1,12 @@
+﻿package com.mapconductor.arcgis
+
+import com.arcgismaps.mapping.view.SceneView
+import com.mapconductor.arcgis.map.ArcGISMapViewController
+import com.mapconductor.arcgis.map.WrapSceneView
+import com.mapconductor.core.map.MapViewHolderInterface
+import com.mapconductor.core.map.StaticHolder
+
+typealias ArcGISMapViewHolderInterface = MapViewHolderInterface<WrapSceneView, SceneView>
+
+object ArcGISViewControllerStore :
+    StaticHolder<ArcGISMapViewController>()
